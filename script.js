b941b99fe13be86bb9e2817500b0ac55e67167f6
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2021-10-04 23:25:26 +0200

    update

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-color1.addEventListener("input", function () {
+function setGradient() {
     body.style.background =
         "linear-gradient(to right, "
         + color2.value
         + ", "
         + color2.value
         + ")";
-})
 
-color2.addEventListener("input", function () {
-    body.style.background =
-        "linear-gradient(to right, "
-        + color2.value
-        + ", "
-        + color2.value
-        + ")";
-})
+}
+
+color1.addEventListener("input", setGradient);
+
+
+
+color2.addEventListener("input", setGradient);
+
+
